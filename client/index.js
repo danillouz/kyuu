562d34e03d5fa6fba6962f9e7441ad9af5121d72
Daniël Illouz <daniel.illouz@me.com>
2018-10-15 23:01:57 +0200

    Stream Audio/Video locally

--- client/index.js
+++ client/index.js
@@ -4,27 +4,134 @@ const signalingServer = {
   //
 };
 
-async function main() {
-  // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-  const mediaConstraints = {
-    audio: false,
-    video: true
-  };
+// https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+const mediaConstraints = {
+  audio: true,
+  video: true
+};
 
-  const localVideo = document.getElementById('local-video');
+const localVideo = document.getElementById('local-video');
+const remoteVideo = document.getElementById('remote-video');
+const startCallButton = document.getElementById('start-call');
+const endCallButton = document.getElementById('end-call');
 
-  // MediaStream: https://developer.mozilla.org/en-US/docs/Web/API/Media_Streams_API
-  const stream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+let localConn;
+let remoteConn;
 
-  console.log('stream: ', stream);
+let localStream;
+let remoteStream;
 
-  const [videoTracks] = stream.getVideoTracks();
+let localVideoTracks;
+let localAudioTracks;
 
-  console.log('video tracks: ', videoTracks);
-
-  localVideo.srcObject = stream;
+function _getOtherPeer(conn) {
+  return conn === localConn ? remoteConn : localConn;
 }
 
-main().catch(err => {
-  console.log('ERROR: ', err);
-});
+function _createPeerConnection(servers) {
+  const peerConnection = new RTCPeerConnection(servers);
+
+  peerConnection.addEventListener('icecandidate', async event => {
+    const { target: conn, candidate } = event;
+
+    console.log('event [icecandidate] connection: ', conn);
+
+    if (candidate) {
+      const newCandidate = new RTCIceCandidate(candidate);
+      const otherPeer = _getOtherPeer(conn);
+
+      await otherPeer.addIceCandidate(newCandidate);
+
+      console.log('added ICE candidate');
+    }
+  });
+
+  peerConnection.addEventListener('iceconnectionstatechange', event => {
+    const { target: conn } = event;
+
+    console.log('event [iceconnectionstatechange] connection:', conn);
+
+    //
+  });
+
+  peerConnection.addEventListener('addstream', event => {
+    console.log('event [addstream] stream:', event.stream);
+
+    const { stream } = event;
+
+    remoteVideo.srcObject = stream;
+
+    remoteStream = stream;
+  });
+
+  return peerConnection;
+}
+
+async function startCall(event) {
+  console.log('start call');
+
+  startCallButton.disabled = true;
+  endCallButton.disabled = false;
+
+  // MediaStream: https://developer.mozilla.org/en-US/docs/Web/API/Media_Streams_API
+  localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+
+  localVideo.srcObject = localStream;
+
+  const [videoTracks] = localStream.getVideoTracks();
+  const [audioTracks] = localStream.getAudioTracks();
+
+  if (videoTracks) {
+    console.log(`using video device ${videoTracks.label}`);
+
+    localVideoTracks = videoTracks;
+  }
+
+  if (audioTracks) {
+    console.log(`using audio device ${audioTracks.label}`);
+
+    localAudioTracks = audioTracks;
+  }
+
+  const servers = null;
+
+  localConn = _createPeerConnection(servers);
+  remoteConn = _createPeerConnection(servers);
+
+  console.log('local conn: ', localConn);
+  console.log('remote conn: ', remoteConn);
+
+  localConn.addStream(localStream);
+
+  const offer = await localConn.createOffer();
+
+  await localConn.setLocalDescription(offer);
+  await remoteConn.setRemoteDescription(offer);
+
+  const answer = await remoteConn.createAnswer();
+
+  await remoteConn.setLocalDescription(answer);
+  await localConn.setRemoteDescription(answer);
+}
+
+async function endCall(event) {
+  console.log('end call');
+
+  remoteConn.close();
+  localConn.close();
+
+  remoteStream = null;
+  localStream = null;
+
+  remoteVideo.srcObject = null;
+  localVideo.srcObject = null;
+
+  localVideoTracks.stop();
+  localAudioTracks.stop();
+
+  startCallButton.disabled = false;
+  endCallButton.disabled = true;
+}
+
+startCallButton.addEventListener('click', startCall);
+endCallButton.addEventListener('click', endCall);
